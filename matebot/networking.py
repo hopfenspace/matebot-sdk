b58175ab401928b564dc69ad39f06929a2365434
myOmikron <accounts@omikron.dev>
2021-12-28 21:20:20 +0100

    Fixed get requests

--- matebot/networking.py
+++ matebot/networking.py
@@ -23,7 +23,9 @@ class Network:
         self.client = httpx.AsyncClient(verify=self.config.verify)
 
     async def make_request(self, verb: Http, address: str, data=None):
-        headers = {"Authorization": f"RCP {rc_protocol.get_checksum({}, self.config.secret, salt='/' + address)}"}
+        if not data:
+            data = {}
+        headers = {"Authorization": f"RCP {rc_protocol.get_checksum(data, self.config.secret, salt='/' + address)}"}
         if verb == Http.GET:
             response = await self.client.get(
                 os.path.join(self.config.uri, address),
@@ -44,7 +46,10 @@ class Network:
 
         if response.status_code == 200 or response.status_code == 201:
             decoded = json.loads(response.text)
-            return decoded
+            if decoded["success"]:
+                return decoded
+            else:
+                print(decoded["info"])
         else:
             try:
                 pprint(json.loads(response.text))
