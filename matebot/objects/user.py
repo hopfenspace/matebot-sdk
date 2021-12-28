990f09bf4730e98ec9a61ca611eefd0bb5412146
myOmikron <accounts@omikron.dev>
2021-12-28 21:31:21 +0100

    Updated user model to match current api

--- matebot/objects/user.py
+++ matebot/objects/user.py
@@ -7,17 +7,18 @@ from matebot.objects.base import Base
 class User(Base):
     """This class represents a user."""
     def __init__(
-            self, modified: int, created: int, complete_access: bool, external: bool, identifier: int, active: bool,
-            name: str, user_alias_ids: list, balance: int, voucher_id: Union[list, None]
+            self, modified: int, created: int, internal: bool, identifier: int, active: bool,
+            name: str, user_alias_ids: dict[int, str], balance: int, voucher_id: Union[list, None],
+            vouched_for: Union[list, None]
     ):
         super().__init__()
         self.identifier = identifier
         self.name = name
-        self.complete_access = complete_access
         self.active = active
         self.balance = balance
         self.created = datetime.utcfromtimestamp(created)
         self.modified = datetime.utcfromtimestamp(modified)
-        self.external = external
+        self.internal = internal
         self.voucher_id = voucher_id
         self.user_alias_ids = user_alias_ids
+        self.vouched_for = vouched_for
