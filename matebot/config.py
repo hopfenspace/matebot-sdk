ab419c0f4bf5b6e0084a2a4070c9eac0bc30dda5
myOmikron <accounts@omikron.dev>
2021-12-29 14:49:43 +0100

    Added application_id to config

--- matebot/config.py
+++ matebot/config.py
@@ -4,6 +4,7 @@ import staticconfig
 class Config(staticconfig.Config):
     def __init__(self):
         super().__init__()
+        self.application_id = ""
         self.uri = ""
         self.secret = ""
         self.verify = True
